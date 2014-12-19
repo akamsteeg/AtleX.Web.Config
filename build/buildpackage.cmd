7eb838e841bcc0f3acddf66f70fb40938fde1251
Alex Kamsteeg <alex@atlex.nl>
2014-12-20 00:48:38 +0100

    More robust build script

--- build/buildpackage.cmd
+++ build/buildpackage.cmd
@@ -1,3 +1,8 @@
-# Build NuGet package
+:: Clean the output folder
+if exist .\output rm .\output\*.*
 
+:: Create the output folder, if it doesn't exist
+if not exist .\output md .\output
+
+:: Build NuGet package
 .\tools\nuget pack ..\src\AtleX.Web.Config.nuspec -OutputDirectory .\output\
